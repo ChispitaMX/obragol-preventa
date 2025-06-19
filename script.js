6b9ac084ff972a62fc871ddfe4a5eaea3c2ffbad
ChispitaMX <salvador671221@gmail.com>
2025-06-19 10:32:52 -0600

    VERSION OBRAGOL 0.001

--- script.js
+++ script.js
@@ -1,2 +1,1 @@
-// Este script manejará la lógica de conexión, compra y conteo
-console.log("Script conectado correctamente. Se implementará la lógica real en producción.");
+// script.js: JS logic for wallet connection and purchase
