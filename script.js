314981f3b47280cb1b170f6bcd886a49a6ea7c48
ChispitaMX <salvador671221@gmail.com>
2025-06-19 11:09:13 -0600

    OBRAGOL 0.001

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// script.js: JS logic for wallet connection and purchase
+
+document.getElementById('connectButton').addEventListener('click', async () => {
+  alert("OBRAGOL Token is requesting connection 🚓");
+});
+
+document.getElementById('buyButton').addEventListener('click', async () => {
+  alert("Transaction signed! Your OBRAGOL Tokens will be sent after presale ends.");
+});
